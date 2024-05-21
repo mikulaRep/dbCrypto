50bc78cdc52d987a7d64b1b7ff4dca181ba68182
Jakub Mikula <kba.mikula@gmail.com>
2024-05-21 13:08:47 +0200

    Adding error and db_manager schemas

--- src/db_manager.rs
+++ src/db_manager.rs
@@ -1,1 +1,17 @@
+use crate::error::CryptoResult;
+use rusqlite::Connection;
 
+pub struct DbManager {
+    conn: Connection,
+}
+
+impl DbManager {
+    pub fn new(name: &str) -> CryptoResult<Self> {
+        let conn = Connection::open(name)?;
+        Ok(Self { conn })
+    }
+
+    pub fn conn(&self) -> &Connection {
+        &self.conn
+    }
+}
--- src/error.rs
+++ src/error.rs
@@ -1,1 +1,32 @@
+use rusqlite::Error as RusqliteError;
+use std::env::VarError;
+use std::io::Error as IoError;
+use std::num::ParseIntError;
+use thiserror::Error;
 
+pub type CryptoResult<T> = Result<T, CryptoError>;
+
+#[derive(Debug, Error)]
+pub enum CryptoError {
+    // Rusqlite
+    #[error("{0}")]
+    Rusqlite(#[from] RusqliteError),
+
+    // Std
+    #[error("{0}")]
+    Io(#[from] IoError),
+    #[error("{0}")]
+    ParseInt(#[from] ParseIntError),
+    #[error("{0}")]
+    Var(#[from] VarError),
+
+    // Generic
+    #[error("{0}")]
+    GenericErr(String),
+}
+
+impl CryptoError {
+    pub fn generic_err(msg: impl Into<String>) -> Self {
+        CryptoError::GenericErr(msg.into())
+    }
+}
